d0dad5c8e8411cf2907e3a309c8614a17c6345aa
emir seçer <emrsecere3@gmail.com>
2023-09-16 23:12:31 +0300

    Initial Commit

---
+++ haberuygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  haberuygulamasi
+//
+//  Created by Emir Seçer on 16.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
